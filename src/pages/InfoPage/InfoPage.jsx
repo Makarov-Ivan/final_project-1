46219750acac4f52b3c50a0c3af04d7c8d057cfb
Makarov-Ivan <makivanik@gmail.com>
2020-05-16 23:50:29 +0300

    fetchind staff data

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const InfoPage = () => {
+  let [staff, setStaff] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5&gender=female&nat=US")
+      .then((res) => res.json())
+      .then(({ results }) => {
+        setStaff(results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(staff);
+
   return (
     <div className='infoPage'>
       <h2>This is info page</h2>
+      <ul>
+        {staff.map((item, i) => {
+          return (
+            <li key={i}>
+              {item.name.first}
+              {item.name.last}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
