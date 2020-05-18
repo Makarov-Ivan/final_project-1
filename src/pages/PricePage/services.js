89ef9c243b3d05331e08c0b9206d1e9b8fffadae
Makarov-Ivan <makivanik@gmail.com>
2020-05-18 14:10:23 +0300

    some services added

--- src/pages/PricePage/services.js
+++ src/pages/PricePage/services.js
@@ -7,6 +7,8 @@ export const services = [
     serviceImg:
       "https://drscdn.500px.org/photo/209600887/m%3D900/v2?sig=37d6d674aab3ce3cde4c59fe0ead30143a0054458adbef830fedf95771e81745",
     serviceDescription: "Solarium Description",
+    serviceDescriptionFull:
+      "Солярий, аэросолярий — специально оборудованная открытая площадка для проведения дозированных облучений прямым или рассеянным излучением, полученным от специальных эритемных ультрафиолетовых ламп, в стекло колбы которых вводятся специальные добавки, не пропускающие опасную коротковолновую часть УФ-спектра.",
   },
   {
     serviceType: 1,
@@ -15,6 +17,37 @@ export const services = [
     serviceImg:
       "https://drscdn.500px.org/photo/1003273147/m%3D900/v2?sig=53566f689dba218d3022c6f8858cc8b652ab100f2167d413167ac7160f9ffeb2",
     serviceDescription: "Massage Description",
+    serviceDescriptionFull:
+      "Масса́ж — совокупность приёмов механического и рефлекторного воздействия на ткани и органы в виде растирания, давления, вибрации, проводимых непосредственно на поверхности тела человека как руками, так и специальными аппаратами через воздушную, водную или иную среду с целью достижения лечебного или иного эффекта. ",
+  },
+  {
+    serviceType: 1,
+    serviceName: "Иглоука́лывание",
+    servicePrice: 1500,
+    serviceImg:
+      "https://magazinemood.com/wp-content/uploads/2019/04/akupunkturnyy-feyslifting-1-1160x653.jpg",
+    serviceDescription: "Иглоукалывание - воздействие на акупунктурные точки",
+    serviceDescriptionFull:
+      "Иглоукалывание – это процедура воздействия на специальные акупунктурные точки организма в тканях на разной глубине, которое может затрагивать опорно-двигательный аппарат. В зависимости от требуемого лечебного эффекта, иголки в точки вводят с различной интенсивностью и длительностью воздействия. Результатом лечения может быть купирование и устранение боли.",
+  },
+  {
+    serviceType: 1,
+    serviceName: "Ботокс для лица",
+    servicePrice: 1500,
+    serviceImg: "https://estetmedcenter.ru/upload/image/img7.jpg",
+    serviceDescription: "Инъекции ботокса",
+    serviceDescriptionFull:
+      "Несколько точных инъекций ботокса, диспорта или ксеомина – и черты лица могут преобразиться. Это препараты на основе ботулотоксина типа А. Ботулотоксины так давно применяются в косметологии, что о их действии знают даже те, кто далек от данной темы.",
+  },
+  {
+    serviceType: 1,
+    serviceName: "Создание прически",
+    servicePrice: 1500,
+    serviceImg:
+      "https://upload.wikimedia.org/wikipedia/commons/a/ac/Traditional_Japanese_wedding_hairstyle.jpg",
+    serviceDescription: "Укладка и т.д.",
+    serviceDescriptionFull:
+      "Причёска — это форма, придаваемая волосам стрижкой, завивкой, укладкой и филировкой, подбором цветовой гаммы. Она может быть из естественных и искусственных волос с шиньонами и прядями разных цветов.",
   },
 ];
 // https://images.unsplash.com/photo-1531604269568-ad1b8de0a633?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1533&q=80
