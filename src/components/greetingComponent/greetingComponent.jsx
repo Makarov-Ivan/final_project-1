6cb990f13e090e2d7cc6724c2469d36bb0ee8660
Makarov-Ivan <makivanik@gmail.com>
2020-05-20 17:25:12 +0300

    greeting component

---
+++ src/components/greetingComponent/greetingComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./greetingComponent.scss";
+
+export const GreetingComponent = () => {
+  return (
+    <h1 className='greeting'>
+      Салон красоты <span>Barber</span> в Санкт-Петербурге
+    </h1>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GreetingComponent } from "../../components/greetingComponent/greetingComponent";
 
 export const HomePage = () => {
   return (
     <div className='homePage'>
       <h2>This is home page</h2>
+      <GreetingComponent />
     </div>
   );
 };
