dd6c35149899fccffc41161e49f4c0d8b1e89044
Makarov-Ivan <makivanik@gmail.com>
2020-05-26 01:32:15 +0300

    prevent default in registration form

--- src/components/registrationComponent/registrationComponent.jsx
+++ src/components/registrationComponent/registrationComponent.jsx
@@ -7,10 +7,16 @@ export const Registration = () => {
   return (
     <React.Fragment>
       <p>this is registration form</p>
-      {arr.map((item, i) => {
-        return <Input key={i} {...item} />;
-      })}
-      <input type='submit' value='Зарегистрироваться' />
+      <form
+        action=''
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+        {arr.map((item, i) => {
+          return <Input key={i} {...item} />;
+        })}
+        <input type='submit' value='Зарегистрироваться' />
+      </form>
     </React.Fragment>
   );
 };
