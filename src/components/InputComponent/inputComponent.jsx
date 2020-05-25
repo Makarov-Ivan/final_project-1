28c4dbf55f53fdb042351bd29b8ecd8e63323428
Makarov-Ivan <makivanik@gmail.com>
2020-05-26 01:47:50 +0300

    inputs now are required

--- src/components/InputComponent/inputComponent.jsx
+++ src/components/InputComponent/inputComponent.jsx
@@ -4,7 +4,7 @@ export const Input = ({ label, name, type, value }) => {
   return (
     <div className='input'>
       {label ? <label htmlFor={name}>{label}</label> : null}
-      <input type={type} name={name} id={name} />
+      <input type={type} name={name} id={name} required />
     </div>
   );
 };
